a1e7c3ef5cae98a241b7ef44fc7036628a1d9c9d
Pedro Henrique Andreatti <106180196+Pedro-devs@users.noreply.github.com>
2026-09-17 11:57:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function mostrarMensagem(){
-    let nome = document.getElementById("nome").value
-    alert(nome + " Seu cadastro foi feito com sucesso !")
-   
+let usuariocadastro=""
+let senhacadastro=""
+function cadastro(){
+
+usuariocadastro = document.getElementById("usuario").value  
+senhacadastro = document.getElementById("senha").value
+
+if (usuariocadastro == "" || senhacadastro ==""){
+    alert("PREENCHA OS CAMPOS")
+}
+else{
+    alert("CADASTRO FEITO COM SUCESSO")
+}
+
+}
+
+function login(){
+
 }
